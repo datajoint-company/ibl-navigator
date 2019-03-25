ddc6594be30e955fb8b87e6af7aa4a8dc2bf7d50
Maho <msasaki.a@gmail.com>
2019-03-25 18:12:55 -0500

    reorganize views according to SoW

---
+++ src/app/mouse-list/mouse-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mouse-list',
+  templateUrl: './mouse-list.component.html',
+  styleUrls: ['./mouse-list.component.css']
+})
+export class MouseListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
