8680a862cf8dddfb4125031af24871f8fc3c02b1
Maho <msasaki.a@gmail.com>
2019-05-22 15:34:54 -0500

    add responsive layout and plot rendering for contrast heatmap plot

--- src/app/mouse-list/mouse/contrast-heatmap-plot/contrast-heatmap-plot.component.ts
+++ src/app/mouse-list/mouse/contrast-heatmap-plot/contrast-heatmap-plot.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy, ElementRef, ViewChild, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, OnDestroy, ElementRef, ViewChild, Input, Output, EventEmitter, HostListener } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { MousePlotsService } from '../mouse-plots.service';
 
@@ -9,6 +9,8 @@ declare var Plotly: any;
   styleUrls: ['./contrast-heatmap-plot.component.css']
 })
 export class ContrastHeatmapPlotComponent implements OnInit, OnDestroy {
+  d3 = Plotly.d3;
+  newScreenWidth;
   contrastHeatmapPlotIsAvailable: boolean;
   plotConfig = {
     responsive: true,
@@ -45,6 +47,39 @@ export class ContrastHeatmapPlotComponent implements OnInit, OnDestroy {
     }
   };
 
+  mediumScreenDataStyle = {
+    colorbar: { thickness: '7' }
+  };
+  mediumScreenLayout = {
+    font: { size: '10' },
+    width: '460',
+    height: '390'
+  };
+
+  mediumSmallScreenLayout = {
+    font: { size: '10' },
+    width: '412',
+    height: '340'
+  };
+
+  mediumLargeScreenDataStyle = {
+    // colorbar: { thickness: '8.5' }
+  };
+  mediumLargeScreenLayout = {
+    font: { size: '11' },
+    width: '500',
+    height: '420'
+  };
+
+  defaultScreenDataStyle = {
+    // colorbar: { thickness: '10' }
+  };
+  defaultScreenLayout = {
+    font: { size: '12' },
+    width: '',
+    height: ''
+  };
+
   private contrastHeatmapPlotSubscription: Subscription;
 
   @Output() contrastHeatmapPlotAvailability: EventEmitter<any> = new EventEmitter();
@@ -52,7 +87,24 @@ export class ContrastHeatmapPlotComponent implements OnInit, OnDestroy {
   @ViewChild('contrastHeatmapPlot') elem: ElementRef;
   constructor(public mousePlotsService: MousePlotsService) { }
 
+  @HostListener('window:resize', ['$event.target']) onresize(event) {
+    this.newScreenWidth = event.innerWidth;
+    console.log('screen width change - CH plot: ', this.newScreenWidth);
+    const responsiveCHplot = this.d3.select(this.elem.nativeElement).node();
+    if (this.newScreenWidth < 768) {
+      Plotly.update(responsiveCHplot, this.defaultScreenDataStyle, this.defaultScreenLayout);
+    } else if (this.newScreenWidth < 876 && (this.newScreenWidth > 768 || this.newScreenWidth === 768)) {
+      Plotly.update(responsiveCHplot, this.mediumScreenDataStyle, this.mediumSmallScreenLayout);
+    } else if (this.newScreenWidth < 1024 && (this.newScreenWidth > 876 || this.newScreenWidth === 876)) {
+      Plotly.update(responsiveCHplot, this.mediumScreenDataStyle, this.mediumScreenLayout);
+    } else if (this.newScreenWidth < 1440 && (this.newScreenWidth > 1024 || this.newScreenWidth === 1024)) {
+      Plotly.update(responsiveCHplot, this.mediumLargeScreenDataStyle, this.mediumLargeScreenLayout);
+    } else {
+      Plotly.update(responsiveCHplot, this.defaultScreenDataStyle, this.defaultScreenLayout);
+    }
+  }
   ngOnInit() {
+    const screenSizeInitial = window.innerWidth;
     const element = this.elem.nativeElement;
     this.mousePlotsService.getContrastHeatMapPlot({'subject_uuid': this.mouseInfo['subject_uuid']});
     this.contrastHeatmapPlotSubscription = this.mousePlotsService.getContrastHeatmapPlotLoadedListener()
@@ -66,6 +118,15 @@ export class ContrastHeatmapPlotComponent implements OnInit, OnDestroy {
           this.contrastHeatmapPlotAvailability.emit(this.contrastHeatmapPlotIsAvailable);
           this.plotConfig['toImageButtonOptions']['filename'] = this.mouseInfo['subject_nickname'] + '_contrast_heatmap_plot';
           Plotly.newPlot(element, contrastHeatmapPlot['data'], contrastHeatmapPlot['layout'], this.plotConfig);
+          if (screenSizeInitial < 768) {
+            Plotly.update(element, this.defaultScreenDataStyle, this.defaultScreenLayout);
+          } else if (screenSizeInitial < 876 && (screenSizeInitial > 768 || screenSizeInitial === 768)) {
+            Plotly.update(element, this.mediumScreenDataStyle, this.mediumSmallScreenLayout);
+          } else if (screenSizeInitial < 1024 && (screenSizeInitial > 876 || screenSizeInitial === 876)) {
+            Plotly.update(element, this.mediumScreenDataStyle, this.mediumScreenLayout);
+          } else if (screenSizeInitial < 1440 && (screenSizeInitial > 1024 || screenSizeInitial === 1024)) {
+            Plotly.update(element, this.mediumLargeScreenDataStyle, this.mediumLargeScreenLayout);
+          }
         } else {
           console.log('contrast heatmap plot unavailable');
           this.contrastHeatmapPlotIsAvailable = false;
