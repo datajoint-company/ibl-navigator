bc690d0cf07c6fda9f62251a0c0f5839d334ca92
guzman-raphael <raphael.h.guzman@gmail.com>
2019-07-26 16:45:41 -0500

    Add Flask backend to docker-compose up.

---
+++ ibl-frontend/frontend-content/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  api_url: '$PROD_NODE_API',
+  backend_url: '$PROD_NODE_BACKEND'
+};
--- ibl-frontend/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  api_url: 'http://localhost:3333/api',
-  backend_url: 'http://localhost:3333'
-};
