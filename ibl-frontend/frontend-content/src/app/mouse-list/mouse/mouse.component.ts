86fa0a189df3b2fe25dcff5cb153593c3927b04a
Maho <msasaki.a@gmail.com>
2019-07-29 16:46:30 -0500

    remove unneccesary mouse info from going back to restricted sessions

--- ibl-frontend/frontend-content/src/app/mouse-list/mouse/mouse.component.ts
+++ ibl-frontend/frontend-content/src/app/mouse-list/mouse/mouse.component.ts
@@ -12,6 +12,7 @@ export class MouseComponent implements OnInit, OnDestroy {
   public mouse_uuid: string;
   private mouseSubscription: Subscription;
   mouse: any;
+  cleanMouse; // for returning as query param going back to sessions list for the mouse
 
   constructor(private route: ActivatedRoute, public allMiceService: AllMiceService) { }
 
@@ -26,6 +27,17 @@ export class MouseComponent implements OnInit, OnDestroy {
         // console.log('got the mouse with uuid ', this.mouse_uuid);
         // console.log(mouse);
         this.mouse = mouse[0];
+
+        this.cleanMouse = {
+          lab_name: this.mouse.lab_name,
+          session_project: this.mouse.projects,
+          responsible_user: this.mouse.responsible_user,
+          sex: this.mouse.sex,
+          subject_birth_date: this.mouse.subject_birth_date,
+          subject_line: this.mouse.subject_line,
+          subject_nickname: this.mouse.subject_nickname,
+          subject_uuid: this.mouse.subject_uuid
+        };
       });
   }
 
@@ -33,11 +45,6 @@ export class MouseComponent implements OnInit, OnDestroy {
     if (this.mouseSubscription) {
       this.mouseSubscription.unsubscribe();
     }
-  }
-
-  getSessions(mouse) {
-    console.log('show session button clicked!');
-    console.log(mouse);
   }
 
 }
