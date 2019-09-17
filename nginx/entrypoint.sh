43cede34cd70b8518d99b18ac0a8f49ba567eee8
guzman-raphael <raphael.h.guzman@gmail.com>
2019-09-17 15:38:34 -0500

    Update nginx conf destination.

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,9 +1,9 @@
 #! /bin/sh
 
-sed -i "s|{{SUBDOMAINS}}|${SUBDOMAINS}|g" /etc/nginx/site-confs/app.conf
-sed -i "s|{{URL}}|${URL}|g" /etc/nginx/site-confs/app.conf
-sed -i "s|{{NODE_SERVER}}|${NODE_SERVER}|g" /etc/nginx/site-confs/app.conf
-sed -i "s|{{FRONTEND_SERVER}}|${FRONTEND_SERVER}|g" /etc/nginx/site-confs/app.conf
+sed -i "s|{{SUBDOMAINS}}|${SUBDOMAINS}|g" /etc/nginx/conf.d/default.conf
+sed -i "s|{{URL}}|${URL}|g" /etc/nginx/conf.d/default.conf
+sed -i "s|{{NODE_SERVER}}|${NODE_SERVER}|g" /etc/nginx/conf.d/default.conf
+sed -i "s|{{FRONTEND_SERVER}}|${FRONTEND_SERVER}|g" /etc/nginx/conf.d/default.conf
 
 # /init
 # nginx -g daemon off
