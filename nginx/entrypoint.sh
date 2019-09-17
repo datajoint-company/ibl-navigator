7802fc69088940b75a5f478a99fbd70307f3ec9e
guzman-raphael <raphael.h.guzman@gmail.com>
2019-09-17 15:24:44 -0500

    Update where nginx config stored.

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,9 +1,10 @@
-#! /bin/bash
+#! /bin/sh
 
-sed -i "s|{{SUBDOMAINS}}|${SUBDOMAINS}|g" /etc/nginx/nginx.conf
-sed -i "s|{{URL}}|${URL}|g" /etc/nginx/nginx.conf
-sed -i "s|{{NODE_SERVER}}|${NODE_SERVER}|g" /etc/nginx/nginx.conf
-sed -i "s|{{FRONTEND_SERVER}}|${FRONTEND_SERVER}|g" /etc/nginx/nginx.conf
+sed -i "s|{{SUBDOMAINS}}|${SUBDOMAINS}|g" /etc/nginx/site-confs/app.conf
+sed -i "s|{{URL}}|${URL}|g" /etc/nginx/site-confs/app.conf
+sed -i "s|{{NODE_SERVER}}|${NODE_SERVER}|g" /etc/nginx/site-confs/app.conf
+sed -i "s|{{FRONTEND_SERVER}}|${FRONTEND_SERVER}|g" /etc/nginx/site-confs/app.conf
 
 # /init
-nginx -g daemon off
+# nginx -g daemon off
+nginx
