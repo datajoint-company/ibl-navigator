808fecdd1ddeba5c5923bcdaa7304a58c8e06ff9
guzman-raphael <raphael.h.guzman@gmail.com>
2019-09-18 10:57:56 -0500

    Start NGINX attached.

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -6,5 +6,5 @@ sed -i "s|{{NODE_SERVER}}|${NODE_SERVER}|g" /etc/nginx/conf.d/default.conf
 sed -i "s|{{FRONTEND_SERVER}}|${FRONTEND_SERVER}|g" /etc/nginx/conf.d/default.conf
 
 # /init
-# nginx -g daemon off
-nginx
+nginx -g "daemon off;"
+# nginx
