255dbe7de5f5af5a9cf49481a68bbc1e11abc1bd
guzman-raphael <raphael.h.guzman@gmail.com>
2019-09-23 11:25:51 -0500

    Separate main NGINX and letsencrypt renewal services.

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -4,6 +4,7 @@ sed -i "s|{{SUBDOMAINS}}|${SUBDOMAINS}|g" /etc/nginx/conf.d/default.conf
 sed -i "s|{{URL}}|${URL}|g" /etc/nginx/conf.d/default.conf
 sed -i "s|{{NODE_SERVER}}|${NODE_SERVER}|g" /etc/nginx/conf.d/default.conf
 sed -i "s|{{FRONTEND_SERVER}}|${FRONTEND_SERVER}|g" /etc/nginx/conf.d/default.conf
+sed -i "s|{{LETSENCRYPT_SERVER}}|${LETSENCRYPT_SERVER}|g" /etc/nginx/conf.d/default.conf
 
 # /init
 nginx -g "daemon off;"
