d279ffb59b33edae55507f2ef22beef863fa97ab
guzman-raphael <raphael.h.guzman@gmail.com>
2019-09-25 11:47:22 -0500

    Update NGINX to wait for certs to become available and upadte on renewal.

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,11 +1,30 @@
 #! /bin/sh
 
-sed -i "s|{{SUBDOMAINS}}|${SUBDOMAINS}|g" /etc/nginx/conf.d/default.conf
-sed -i "s|{{URL}}|${URL}|g" /etc/nginx/conf.d/default.conf
-sed -i "s|{{NODE_SERVER}}|${NODE_SERVER}|g" /etc/nginx/conf.d/default.conf
-sed -i "s|{{FRONTEND_SERVER}}|${FRONTEND_SERVER}|g" /etc/nginx/conf.d/default.conf
-sed -i "s|{{LETSENCRYPT_SERVER}}|${LETSENCRYPT_SERVER}|g" /etc/nginx/conf.d/default.conf
+sed -i "s|{{SUBDOMAINS}}|${SUBDOMAINS}|g" /etc/nginx/conf.d/base.conf
+sed -i "s|{{URL}}|${URL}|g" /etc/nginx/conf.d/base.conf
+sed -i "s|{{NODE_SERVER}}|${NODE_SERVER}|g" /etc/nginx/conf.d/base.conf
+sed -i "s|{{FRONTEND_SERVER}}|${FRONTEND_SERVER}|g" /etc/nginx/conf.d/base.conf
+sed -i "s|{{LETSENCRYPT_SERVER}}|${LETSENCRYPT_SERVER}|g" /etc/nginx/conf.d/base.conf
+
+sed -i "s|{{SUBDOMAINS}}|${SUBDOMAINS}|g" /ssl.conf
+sed -i "s|{{URL}}|${URL}|g" /ssl.conf
+sed -i "s|{{NODE_SERVER}}|${NODE_SERVER}|g" /ssl.conf
+sed -i "s|{{FRONTEND_SERVER}}|${FRONTEND_SERVER}|g" /ssl.conf
+sed -i "s|{{LETSENCRYPT_SERVER}}|${LETSENCRYPT_SERVER}|g" /ssl.conf
 
 # /init
-nginx -g "daemon off;"
-# nginx
+# nginx -g "daemon off;"
+nginx
+
+while [ ! -d /etc/letsencrypt/archive/${SUBDOMAINS}.${URL} ]; do
+    sleep 5
+done
+
+mv /ssl.conf /etc/nginx/conf.d/ssl.conf
+nginx -s reload
+
+inotifywait -m /etc/letsencrypt/archive/${SUBDOMAINS}.${URL} |
+    while read path action file; do
+        echo "Reloading NGINX since $file issue $action event"
+        nginx -s reload
+    done
