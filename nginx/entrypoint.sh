d6171e0b32918e9d45ee96155a56e5972a329b6b
guzman-raphael <raphael.h.guzman@gmail.com>
2019-09-25 11:53:31 -0500

    Add debug logs.

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -16,15 +16,17 @@ sed -i "s|{{LETSENCRYPT_SERVER}}|${LETSENCRYPT_SERVER}|g" /ssl.conf
 # nginx -g "daemon off;"
 nginx
 
+echo "Waiting for initial certs"
 while [ ! -d /etc/letsencrypt/archive/${SUBDOMAINS}.${URL} ]; do
     sleep 5
 done
 
+echo "Enabling SSL feature"
 mv /ssl.conf /etc/nginx/conf.d/ssl.conf
 nginx -s reload
 
 inotifywait -m /etc/letsencrypt/archive/${SUBDOMAINS}.${URL} |
     while read path action file; do
-        echo "Reloading NGINX since $file issue $action event"
+        echo "Renewal: Reloading NGINX since $file issue $action event"
         nginx -s reload
     done
