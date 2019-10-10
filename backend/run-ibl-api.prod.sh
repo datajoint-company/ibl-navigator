0ad31ffa9c3f7c5f92857a9abfba542de91a0748
guzman-raphael <raphael.h.guzman@gmail.com>
2019-10-10 10:57:51 -0500

    Expose worker count to env var.

--- backend/run-ibl-api.prod.sh
+++ backend/run-ibl-api.prod.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 export FLASK_APP=./iblapi.py
 [ "$1" = "development" ] && args="--log-level=debug" || args=""
-cd "`dirname $0`" && exec gunicorn -t 600 -w 4 -b 0.0.0.0:5000 $args iblapi-gunicorn
+cd "`dirname $0`" && exec gunicorn -t 600 -w $WORKER_COUNT -b 0.0.0.0:5000 $args iblapi-gunicorn
