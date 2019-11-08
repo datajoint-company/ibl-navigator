77851099d697f5a53fca2908b1c948708445bdc4
Maho <msasaki.a@gmail.com>
2019-11-07 18:32:51 -0600

    add project unassigned filter option for sessionpage, adjust docker-compose-dev to work with new setting

--- backend/iblapi.py
+++ backend/iblapi.py
@@ -193,13 +193,18 @@ def handle_q(subpath, args, proj, **kwargs):
     ret = []
     post_process = None
     if subpath == 'sessionpage':
-        q = (acquisition.Session().aggr(
+        sess_proj = acquisition.Session().aggr(
+            acquisition.SessionProject().proj('session_project', dummy2='"x"') * dj.U('dummy2'),
+            session_project='IFNULL(session_project, "unassigned")',
+            keep_all_rows=True
+        )
+        psych_curve = acquisition.Session().aggr(
             # plotting_behavior.SessionPsychCurve(),
             plotting_behavior.SessionPsychCurve().proj(dummy='"x"') * dj.U('dummy'),
             #  nplot='count(distinct(concat(subject_uuid, session_start_time)))',
-             nplot='count(dummy)',
-             keep_all_rows=True)
-             * acquisition.Session() * acquisition.SessionProject() * subject.Subject() * subject.SubjectLab() * subject.SubjectUser()
+            nplot='count(dummy)',
+            keep_all_rows=True)
+        q = (acquisition.Session() * sess_proj * psych_curve * subject.Subject() * subject.SubjectLab() * subject.SubjectUser()
              & ((reference.Lab() * reference.LabMember())
                 & reference.LabMembership().proj('lab_name', 'user_name'))
              & args)
