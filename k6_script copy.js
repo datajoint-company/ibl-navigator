2ec3ccf01768b967b6702ad86081574639ef3d66
guzman-raphael <raphael.h.guzman@gmail.com>
2020-06-30 14:29:54 -0500

    Add an updated docker-compose file.

--- k6_script copy.js
+++ k6_script copy.js
@@ -14,7 +14,7 @@ import http from 'k6/http';
 import { check, group, sleep } from 'k6';
 
 var url = 'https://ec2-35-174-113-72.compute-1.amazonaws.com/api/sessions';
-var bearer = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImlibHVzZXIiLCJpYXQiOjE1OTM1MjgyNzIsImV4cCI6MTU5MzYxNDY3Mn0.DSzIhL1pc9Y_JY19z71UgxcRu_DsSVD7PmiZfCbsyaM';
+var bearer = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImlibHVzZXIiLCJpYXQiOjE1OTM0NTE5MzUsImV4cCI6MTU5MzUzODMzNX0.kStpG9M-C51sIvkFcsaRlE4dqntOoSuP02bZaMT9ny4';
 
 // // export let options = {
 // //     insecureSkipTLSVerify: true,
@@ -52,9 +52,9 @@ var bearer = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImlibHVzZXIiL
 
 
 export let options = {
-    vus: 12,
+    vus: 24,
     // duration: '90s',
-    iterations: 12,
+    iterations: 24,
     insecureSkipTLSVerify: true,
 };
 export default function() {
--- k6_test1.js
+++ k6_test1.js
@@ -1,15 +1,9 @@
 import http from 'k6/http';
 import { check, group, sleep } from 'k6';
 
-var url = 'https://{{TEMP_HOST}}/api/sessions';
-var bearer = '{{TEMP_TOKEN}}';
+var url = 'https://ec2-35-174-113-72.compute-1.amazonaws.com/api/sessions';
+var bearer = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImlibHVzZXIiLCJpYXQiOjE1OTM0NTE5MzUsImV4cCI6MTU5MzUzODMzNX0.kStpG9M-C51sIvkFcsaRlE4dqntOoSuP02bZaMT9ny4';
 
-export let options = {
-    vus: 24,
-    // duration: '90s',
-    iterations: 24,
-    insecureSkipTLSVerify: true,
-};
 
 export default function() {
     group('sessions', () => {
