57bc52eaa39f7496eb6dc7b6d71e2bf28f3b8b39
Maho Sasaki <maho@vathes.com>
2020-10-13 11:40:02 -0500

    adjust the environment variable for deployment and update readme to remind in future

--- ibl-frontend/frontend-content/src/environments/environment.prod.ts
+++ ibl-frontend/frontend-content/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   // api_url: '$PROD_NODE_API',
-  backend_url: 'https://fakeservices.datajoint.io/api'
+  backend_url: '$PROD_NODE_BACKEND'
 };
+// before deploying, make sure backend_url is backend_url: '$PROD_NODE_BACKEND'
--- ibl-frontend/frontend-content/src/environments/environment.ts
+++ ibl-frontend/frontend-content/src/environments/environment.ts
@@ -5,8 +5,14 @@
 export const environment = {
   production: false,
   // api_url: '$DEV_NODE_API',
-  backend_url: 'https://fakeservices.datajoint.io/api'
+  backend_url: '$DEV_NODE_BACKEND'
 };
+
+// export const environment = {
+//   production: false,
+//   // api_url: '$DEV_NODE_API',
+//   backend_url: '$DEV_NODE_BACKEND'
+// };
 
 /*
  * For easier debugging in development mode, you can import the following file
