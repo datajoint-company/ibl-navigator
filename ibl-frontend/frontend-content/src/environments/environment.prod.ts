e1b4c601df04ea69216bb7d8c2c1c40a10797431
Carlos Ortiz <carlosortiz9204@gmail.com>
2021-11-02 17:40:10 -0500

    Changes made to environment.prod.ts

--- ibl-frontend/frontend-content/src/environments/environment.prod.ts
+++ ibl-frontend/frontend-content/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   // api_url: '$PROD_NODE_API',
-  backend_url: '$PROD_NODE_API'
+  backend_url: '$PROD_NODE_BACKEND'
 };
 // before deploying, make sure backend_url part is backend_url: '(dollar)PROD_NODE_BACKEND'
