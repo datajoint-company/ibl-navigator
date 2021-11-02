650068e46282acc5e1ae0df77f0442a157ac9ddc
Carlos Ortiz <carlosortiz9204@gmail.com>
2021-11-02 17:37:03 -0500

    Changes meant to for updateDev

--- ibl-frontend/frontend-content/src/environments/environment.prod.ts
+++ ibl-frontend/frontend-content/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   // api_url: '$PROD_NODE_API',
-  backend_url: 'https://fakeservices.datajoint.io/api'
+  backend_url: '$DEV_NODE_API'
 };
 // before deploying, make sure backend_url part is backend_url: '(dollar)PROD_NODE_BACKEND'
